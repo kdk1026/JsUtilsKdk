be328626d8ff708c9a51b54295a7d8e8d8fc7c36
kdk <kdk1026@gmail.com>
2021-07-01 23:29:35 +0900

    Ajax 관련 전면 수정

--- JsUtilsKdk/commonJquery.js
+++ JsUtilsKdk/commonJquery.js
@@ -1,32 +1,39 @@
 /*
-[Ajax Global 설정 참고]
-    - 헤더 fragment 등에서 선언
-
-$(function(){
-    $.ajaxSetup({
-        beforeSend: function(xhr) {
-			// JWT, oAuth 등 인증
-			xhr.setRequestHeader('Authorization', authorization);
-			
+$.ajaxSetup({
+	beforeSend: function(xhr, settings) {
+		// JWT, oAuth 등 인증
+		xhr.setRequestHeader('Authorization', authorization);
+		
+    	if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {			
 			// Spring Security의 CSRF 사용 시
 			xhr.setRequestHeader(csrfHeader, csrfToken);
-			
-			// 로딩 이미지 Show
-		},
-		, complete: function() {
-			// 로딩 이미지 Hide
-		}
-    });
+        }
+
+		// Spring Security의 AccessDeniedHandler 인터페이스 구현 시, Ajax 호출 여부 판단
+    	xhr.setRequestHeader("X-Ajax-Call", "TRUE");
+		
+		// 로딩 이미지 Show (ajaxStart 에서도 가능)
+    }, complete: function() {
+		// 로딩 이미지 Hide (ajaxStop 에서도 가능)
+	}
+});
+
+$(document).ajaxStart(function() {
+	// 로딩 이미지 Show
+});
+
+$(document).ajaxStop(function() {
+	// 로딩 이미지 Hide
 });
 */
 
 /**
  * @author 김대광 <daekwang1026&#64;gmail.com>
  * @since 2018.12.02
- * @version 1.0
+ * @version 2.0
  * @description 특정 프로젝트가 아닌, 범용적으로 사용하기 위한 jQuery 확장
  */
-$.extend({
+ $.extend({
 	/**
 	 * 엔터키 이벤트 발생 시, 다음 포커스로 이동
      * @param {Element} $element 
@@ -39,157 +46,109 @@ $.extend({
 			}
 		});
 	},
+
     /**
-     * 공통 AJAX Sync 처리
-     *  - 요청: default (application/x-www-form-urlencoded; charset=UTF-8')
+     * 공통 Ajax 처리
+     * @param {boolean} isAsync 
+     * @param {string} method 
      * @param {string} url 
      * @param {(undefined|Object)} header 
-     * @param {(undefined|Object)} param
+     *   - callback 있는 경우, 없으면 {}
+     * @param {(undefined|Object)} param 
      * @param {(undefined|Function)} callback 
+     * @returns 
      */
-    ajaxSync: function(url, header, param, callback) {
-        var _retObj = {};
+	commonAjax: function(isAsync, method, url, header, param, callback) {
+		let _retData = {};
+
+		let _contentType = "application/x-www-form-urlencoded; charset=utf-8";
+		let _params = (param == undefined) ? {} : param;
+
+		if (typeof param == 'object') {
+			try {
+				_params = param.serialize();
+
+			} catch (e) {
+				if (Array.isArray(param)) {
+					_contentType = "application/json; charset=utf-8";
+					_params = JSON.stringify(param);
+				}
+			}
+		}
 
 		$.ajax({
-            url: url,
-            async: false,
-            cache: false,
-            traditional: true,
+			async: isAsync,
+			cache: false,
+			traditional: true,
+			contentType: _contentType,
+			dataType: "json",
+			type: method,
+			url: url,
             headers: (header == undefined) ? {} : header,
-            data: (param == undefined) ? {} : param,
-			type: 'POST',
+			data: _params,
 			success: function(result) {
-                if (callback == undefined) {
-                    _retObj = result;
-                } else {
-                    callback(result);
-                }
+				if (isAsync) {
+					callback(result);
+				} else {
+					if ( (callback == undefined) || (typeof callback != 'function') ) {
+						_retData = result;
+					} else {
+						callback(result);
+					}
+				}
 			},
 			error: function(xhr, status, err) {
-                var _ret = xhr.responseJSON;
-                if ( _ret != null && _ret.replace(/ /gi,'') != '' ) {
-                    _retObj = _ret;
-                } else {
-                    alert('[code] - '+xhr.status+', [error] - '+err);
-                }
+				alert("code = "+ xhr.status + " message = " + xhr.responseText + " error = " + err);
 			}
-        });
-        return _retObj;
-    },
+		});
+
+		return _retData;
+	},
     /**
-     * 공통 AJAX Sync 파일 처리
-     *  - 요청: default (application/x-www-form-urlencoded; charset=UTF-8')
+     * 공통 Ajax 파일 처리
+     * @param {boolean} isAsync 
+     * @param {string} method 
      * @param {string} url 
-     * @param {(undefined|Object)} header
+     * @param {(undefined|Object)} header  
+     *   - callback 있는 경우, 없으면 {}
      * @param {Element} $formElement 
      * @param {(undefined|Function)} callback 
+     * @returns 
      */
-    ajaxSyncFile: function(url, header, $formElement, callback) {
-        var _form = $formElement;
-        var _data = new FormData(_form);
-        var _retObj = {};
+	commonAjaxFile: function(isAsync, method, url, header, $formElement, callback) {
+		let _retData = {};
+
+		let _param = new FormData($formElement[0]);
 
 		$.ajax({
-            url: url,
-            async: false,
-            cache: false,
-            traditional: true,
-            contentType : false,
-            processData : false,
-            headers: (header == undefined) ? {} : header,
-            data: _data,
-			type: 'POST',
+			async: isAsync,
+			cache: false,
+			traditional: true,
+			contentType: false,
+			processData : false,
+			dataType: "json",
+			type: method,
+			url: url,
+			data: _param,
 			success: function(result) {
-                if (callback == undefined) {
-                    _retObj = result;
-                } else {
-                    callback(result);
-                }
+				if (isAsync) {
+					callback(result);
+				} else {
+					if ( (callback == undefined) || (typeof callback != 'function') ) {
+						_retData = result;
+					} else {
+						callback(result);
+					}
+				}
 			},
 			error: function(xhr, status, err) {
-                var _ret = xhr.responseJSON;
-                if ( _ret != null && _ret.replace(/ /gi,'') != '' ) {
-                    _retObj = _ret;
-                } else {
-                    alert('[code] - '+xhr.status+', [error] - '+err);
-                }
+				alert("code = "+ xhr.status + " message = " + xhr.responseText + " error = " + err);
 			}
-        });
-        return _retObj;
-    },
-    /**
-     * 공통 AJAX Sync 처리
-     *  - 요청: JSON
-     * @param {string} url 
-     * @param {(undefined|Object)} header 
-     * @param {(undefined|Object)} param
-     * @param {(undefined|Function)} callback 
-     */
-    ajaxSyncJson: function(url, header, param, callback) {
-        var _retObj = {};
+		});
 
-		$.ajax({
-            url: url,
-            async: false,
-            cache: false,
-            traditional: true,
-            contentType: "application/json; charset=UTF-8",
-            headers: (header == undefined) ? {} : header,
-            data: (param == undefined) ? {} : param,
-			type: 'POST',
-			success: function(result) {
-                if (callback == undefined) {
-                    _retObj = result;
-                } else {
-                    callback(result);
-                }
-			},
-			error: function(xhr, status, err) {
-                var _ret = xhr.responseJSON;
-                if ( _ret != null && _ret.replace(/ /gi,'') != '' ) {
-                    _retObj = _ret;
-                } else {
-                    alert('[code] - '+xhr.status+', [error] - '+err);
-                }
-			}
-        });
-        return _retObj;
-    },
-    /**
-     * 공통 AJAX Sync 처리
-     *  - 응답: HTML
-     * @param {string} url 
-     * @param {(undefined|Object)} header 
-     * @param {(undefined|Object)} param
-     * @param {Element} $divElement
-     */
-    ajaxSyncHtml: function(url, header, param, $divElement) {
-        var _retObj = {};
-
-		$.ajax({
-            url: url,
-            async: false,
-            cache: false,
-            traditional: true,
-            dataType: 'html',
-            headers: (header == undefined) ? {} : header,
-            data: (param == undefined) ? {} : param,
-			type: 'POST',
-			success: function(result) {
-                $divElement.html(result);
-			},
-			error: function(xhr, status, err) {
-                var _ret = xhr.responseJSON;
-                if ( _ret != null && _ret.replace(/ /gi,'') != '' ) {
-                    _retObj = _ret;
-                } else {
-                    alert('[code] - '+xhr.status+', [error] - '+err);
-                }
-			}
-        });
-    },
+		return _retData;
+	},
     
-
     /**
      * <pre>
      * form의 데이터를 Object 형태로 반환
