be4e0a5c1df3e8d2abcf9b7ace2a194dab9a835c
kdk1026 <kdk1026@naver.com>
2022-08-12 12:41:51 +0900

    수정

--- JsUtilsKdk/common.js
+++ JsUtilsKdk/common.js
@@ -1494,8 +1494,8 @@ CommonJS.Byte = {
         const _textLen = _textVal.length;
 
         let _totalByte = 0;
-        for (let i=0; i < textLen; i++) {
-            const eachChar = textVal.charAt(i);
+        for (let i=0; i < _textLen; i++) {
+            const eachChar = _textVal.charAt(i);
 			const uniChar = escape(eachChar);
 
             if ( uniChar.length > 4 ) {
