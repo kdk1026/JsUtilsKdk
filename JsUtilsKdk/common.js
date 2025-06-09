1d0fbe49e8a2bdf21893154b7b43151c1081be04
kdk1026 <kdk1026@gmail.com>
2025-06-09 20:00:16 +0900

    getBrowser 추가가

--- JsUtilsKdk/common.js
+++ JsUtilsKdk/common.js
@@ -2183,6 +2183,41 @@ CommonJS.BrowserInfo = {
     getPublicIp: function() {
         const retJson = CommonJS.Http.commonAjax(false, 'get', 'https://api.ipify.org?format=json', null, {}, null);
         return retJson.ip;
+    },
+    /**
+     * UserAgent에서 브라우저 식별
+     * @returns 
+     */
+    getBrowser: function() {
+        const userAgent = navigator.userAgent.toLowerCase();
+
+        if (userAgent == null) {
+            return "Unknown";
+        }
+    
+        const browserMap = [
+            { name: "Samsung Internet", keywords: ["samsungbrowser"] },
+            { name: "Whale", keywords: ["whale"] },
+            { name: "Microsoft Edge", keywords: ["edge"] },
+            { name: "Opera", keywords: ["opr", "opera"] },
+            { name: "Chrome", keywords: ["chrome"] },
+            { name: "Firefox", keywords: ["firefox"] },
+            { name: "Safari", keywords: ["safari"] },
+            { name: "Internet Explorer", keywords: ["msie", "trident"] }
+        ];
+    
+        for (const browser of browserMap) {
+            for (const keyword of browser.keywords) {
+                if (userAgent.includes(keyword)) {
+                    if (browser.name === "Safari" && userAgent.includes("chrome")) {
+                        continue; // Chrome이 포함되어 있으면 Safari로 간주하지 않음
+                    }
+                    return browser.name;
+                }
+            }
+        }
+    
+        return "Other";
     }
 };
 
